68b3de351e9cccce9c2ff9b639d2af6b3f51c5c9
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-11-15 15:55:00 -0800

    Enemy manager now global variable, missiles started

---
+++ Scenes/Weapons/Missile.gd
@@ -0,0 +1,27 @@
+extends Node
+class_name Missile
+
+@export_category("Game Rules")
+@export var movement_speed : float = 1.0
+@export var damage : float = 10.0
+@export var acceleration : float = 0.0
+@export var life_time : float = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#Play noise
+	#Start Particles
+	#Discover target
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_class("Enemy"):
+		body.take_damage(damage)
+		# Deploy particle effect and sound
+		queue_free()

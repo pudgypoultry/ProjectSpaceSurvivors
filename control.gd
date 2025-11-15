d99ae806ebe39f4ebc032c393c72da07e93112ce
bbkkpluspplus <austinjgerardo@gmail.com>
2025-11-15 13:22:40 -0800

    Main menu
    Main Menu Start/Quit buttons

---
+++ control.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+	
+
+
+
+func _on_quitbutton_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.

23a85b1460ea0861ee250277e9b4e52175c1bf3f
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-11-24 16:29:19 -0800

    Created scripts for list of passives

--- Experimental/StatManager.gd
+++ Experimental/StatManager.gd
@@ -4,13 +4,33 @@ var aggregation : Dictionary = {}
 var playerShip : PlayerController
 
 func Refresh():
+	# Fusion Reactor
 	aggregation["damage"] = 1.0
+	# Power Cells
 	aggregation["duration"] = 1.0
+	# Targeting System
 	aggregation["cooldown"] = 1.0
+	# Radar
 	aggregation["area"] = 1.0
+	# Weapons Systems
 	aggregation["projectileSpeed"] = 1.0
+	# Fabricator
 	aggregation["projectileAmount"] = 1.0
-
+	
+	# Chassis
+	aggregation["health"] = 1.0
+	# Armored Hull
+	aggregation["armor"] = 1.0
+	# Thrusters
+	aggregation["speed"] = 1.0
+	# Machine Learning
+	aggregation["experience"] = 1.0
+	# Repair Bots
+	aggregation["repair"] = 1.0
+	# Inertia Dampeners
+	aggregation["inertia"] = 1.0
+	# Tractor Beam
+	aggregation["pickupRange"] = 1.0
 
 func StartGame(playerObject):
 	playerShip = playerObject
---
+++ Passives/WeaponsSystems.gd
@@ -0,0 +1,22 @@
+extends EquipmentPassive
+
+func _ready():
+	super._ready()
+
+func OnLevelUp(currentLevel : int):
+	print("leveling up to level: " + str(currentLevel + 1))
+	match currentLevel:
+		2:
+			level += 1
+			statChanges["projectileSpeed"] += 0.5
+		3:
+			level += 1
+			statChanges["projectileSpeed"] += 0.5
+		4:
+			level += 1
+			statChanges["projectileSpeed"] += 0.5
+		5:
+			level += 1
+			statChanges["projectileSpeed"] += 0.5
+		_:
+			pass
